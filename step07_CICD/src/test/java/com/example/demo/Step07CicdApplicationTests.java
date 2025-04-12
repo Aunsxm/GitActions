0dbb4f60635321dc3d2ac9db2ca3ff32ae1e8087
Aunsxm <tngus893716@gmail.com>
2025-04-13 01:00:51 +0900

    [UPDATE]파일업로드

---
+++ step07_CICD/src/test/java/com/example/demo/Step07CicdApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+// 또는
+import static org.junit.jupiter.api.Assertions.assertEquals; // JUnit 5
+@SpringBootTest
+class Step07CicdApplicationTests {
+	@Test
+	public void testAddition() {
+		int result = 12 + 3;
+		System.out.println("****비교 전****");
+		
+		assertEquals(15, result); // 성공: 5 == 5
+		System.out.println("****비교 후****");
+	}
+}
